6d062e0676ccdfbb3cfe5b34d47faae0deb1e091
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-04-09 20:50:27 +0600

    add

--- api/index.py
+++ api/index.py
@@ -5,3 +5,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 from django.core.wsgi import get_wsgi_application  # noqa: E402
 
 application = get_wsgi_application()
+app = application
