c2e9f6474b4e10d525192e4de91cedc236a59a8e
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-04-09 21:12:25 +0600

    add

--- core/settings.py
+++ core/settings.py
@@ -133,7 +133,7 @@ USE_I18N = True
 USE_TZ = True
 
 # STATIC / MEDIA
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
